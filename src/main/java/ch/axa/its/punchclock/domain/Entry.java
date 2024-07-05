a72883558d88f017fea2927624cd4e19546e84ef
Elias.Z <e.zulu@gmx.ch>
2024-07-05 08:48:34 +0200

    severin copied all my things

--- src/main/java/ch/axa/its/punchclock/domain/Entry.java
+++ src/main/java/ch/axa/its/punchclock/domain/Entry.java
@@ -1,36 +1,29 @@
 package ch.axa.its.punchclock.domain;
- 
+
 import java.time.LocalDateTime;
- 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
- 
+import lombok.Data;
+
 @Entity
+@Data
 @Table(name = "entry")
 public class Entry {
- 
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
- 
- 
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
     private LocalDateTime checkIn;
- 
- 
+
+    @Column(nullable = false)
     private LocalDateTime checkOut;
- 
+
     private String description;
- 
-    public Long getId() {
-        return id;
-    }
- 
-    public void setId(Long id) {
-        this.id = id;
-    }
- 
 }
